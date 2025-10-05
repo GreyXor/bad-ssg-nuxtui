046b1950226282651b70ab9f9e253fc6e20a9109
GreyXor <greyxor@protonmail.com>
2025-10-05 16:44:45 +0200

    chore: upgrade dependencies

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,27 @@
 <template>
   <UApp>
     <UDashboardGroup >
-      <DashboardSidebar />
-      <UDashboardSearch />
-      <DashboardPanel />
+  <UDashboardSidebar resizable collapsible>
+    <UContentNavigation :navigation="navigation" highlight />
+
+    <template #footer="{ collapsed }">
+      <UFieldGroup block orientation="horizontal">
+        <UButton color="neutral" variant="subtle" label="Submit" />
+        <UButton color="neutral" variant="outline" label="Cancel" />
+      </UFieldGroup>
+    </template>
+  </UDashboardSidebar>
+  <UDashboardPanel>
+    <template #header>
+      <UDashboardNavbar>
+        <template #leading> <UDashboardSidebarCollapse /> </template
+      ></UDashboardNavbar>
+    </template>
+
+    <template #body>
+      Issue with icons and
+    </template>
+  </UDashboardPanel>
     </UDashboardGroup>
   </UApp>
 </template>
